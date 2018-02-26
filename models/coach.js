14e192f3e92a776898e104117d9b2f86d0556321
tmpritchett <tanyampritchett@gmail.com>
2018-02-25 20:38:30 -0500

    changes to index js

--- models/coach.js
+++ models/coach.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const coachSchema = require('../db/schemas/coachSchema')
 
-// Apply existing schema to a mongoose model names soda.
+// Apply existing schema to a mongoose model names coach.
 // This gives us the ability to use methods like findById, create, etc
 const Coach = mongoose.model('coach', coachSchema)
 
