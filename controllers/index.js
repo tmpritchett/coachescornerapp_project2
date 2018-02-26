7440547e6397d720a81464fe62554b090c3fb793
tmpritchett <tanyampritchett@gmail.com>
2018-02-25 20:15:51 -0500

    made changes to index contrioller

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
-var express = require('coaches corner');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 /* get home page. */
 router.get('/', function(req, res, next) {
